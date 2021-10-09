fac70c5fa581d66da119487845a0a4f16331eabb
JaviCervera <JaviCervera@users.noreply.github.com>
2021-10-09 11:31:33 +0200

    Starting replacement of Lua generation with JS

--- src/generator.cpp
+++ src/generator.cpp
@@ -4,20 +4,6 @@ using namespace std;
 using namespace swan;
 
 string Generator::GenProgram(const vector<string>& functions, const vector<string>& program, const Definitions& _) const {
-    const string headerStr =
-        "function _bool(a) if a ~= false and a ~= nil and a ~= 0 then return true else return false end end\n"
-        "function _and(a, b) if _bool(a) and _bool(b) then return 1 else return 0 end end\n"
-        "function _or(a, b) if _bool(a) or _bool(b) then return 1 else return 0 end end\n"
-        "function _not(a) if _bool(a) then return 0 else return 1 end end\n"
-        "_args = {}\n"
-        "function pico.AddIntArg(v) _args[#_args+1] = v end\n"
-        "function pico.AddFloatArg(v) _args[#_args+1] = v end\n"
-        "function pico.AddStringArg(v) _args[#_args+1] = v end\n"
-        "function pico.Call(f) local ret = pico[f](table.unpack(_args)) _args = {} return ret end\n"
-        "function pico.CallInt(f) return Int(pico.CallFloat(f)) end\n"
-        "function pico.CallFloat(f) return tonumber(pico.Call(f)) end\n"
-        "function pico.CallString(f) return tostring(pico.Call(f)) end\n"
-        "function pico.Callable(f) return type(pico[f]) == \"function\" end\n";
     string functionsStr;
     for (size_t i = 0; i < functions.size(); ++i) {
         functionsStr += functions[i] + "\n";
@@ -26,50 +12,47 @@ string Generator::GenProgram(const vector<string>& functions, const vector<strin
     for (size_t i = 0; i < program.size(); ++i) {
         programStr += program[i];
     }
-    return headerStr + functionsStr + programStr + "\n";
+    return functionsStr + programStr + "\n";
 }
 
 string Generator::GenFunctionDef(const Function& func, const string& block) const {
-    return GenFunctionHeader(func) + block + "end\n";
+    return GenFunctionHeader(func) + " {\n" + block + "}\n";
 }
 
 string Generator::GenStatement(const string& exp) const {
-    return exp + "\n";
+    return exp + ";\n";
 }
 
 string Generator::GenIf(const string& exp, const string& block, const string& elseifs, const string& else_, const string& end) const {
-    return "if _bool(" + exp + ") then\n" + block + elseifs + else_ + end;
+    return "if (" + exp + ") {\n" + block + elseifs + else_ + end;
 }
 
 string Generator::GenElseIf(const string& exp, const string& block) const {
-    return "elseif _bool(" + exp + ") then\n" + block;
+    return "} else if (" + exp + ") {\n" + block;
 }
 
 string Generator::GenElse(const string& block) const {
-    return "else\n" + block;
+    return "} else {\n" + block;
 }
 
 string Generator::GenEnd() const {
-    return "end\n";
+    return "}\n";
 }
 
 string Generator::GenFor(const Var& _, const string& assignment, const string& to, const string& step, const string& block, const string& end) const {
-    const string fixedAssignment = (assignment.find("local ", 0) == 0)
-        ? assignment.substr(6)
-        : assignment;
-    return "for " + fixedAssignment + ", " + to + (block != "" ? (", " + step) : "") + " do\n" + block + end;
+    return "for " + assignment + ", " + to + (block != "" ? (", " + step) : "") + " do\n" + block + end;
 }
 
 string Generator::GenWhile(const string& exp, const string& block, const string& end) const {
-    return "while _bool(" + exp + ") do\n" + block + end;
+    return "while (" + exp + ") {\n" + block + end;
 }
 
 string Generator::GenReturn(int funcType, const string& exp) const {
-    return "return " + exp + "\n";
+    return "return " + exp + ";\n";
 }
 
 string Generator::GenVarDef(const Var& var, int expType, const string& exp, bool isGlobal) const {
-    return (!isGlobal ? "local " : "") + GenAssignment(var, expType, exp);
+    return (!isGlobal ? "var " : "") + GenAssignment(var, expType, exp);
 }
 
 string Generator::GenAssignment(const Var& var, int expType, const string& exp) const {
@@ -78,28 +61,24 @@ string Generator::GenAssignment(const Var& var, int expType, const string& exp) 
 
 string Generator::GenBinaryExp(int expType, const Token& token, const string& left, const string& right) const {
     const string op =
-        (token.type == TOK_OR) ? " or " :
-        (token.type == TOK_AND) ? " and " :
+        (token.type == TOK_OR) ? " || " :
+        (token.type == TOK_AND) ? " && " :
         (token.type == TOK_EQUAL) ? " == " :
-        (token.type == TOK_NOTEQUAL) ? " ~= " :
+        (token.type == TOK_NOTEQUAL) ? " != " :
         (token.type == TOK_LESSER) ? " < " :
         (token.type == TOK_LEQUAL) ? " <= " :
         (token.type == TOK_GREATER) ? " > " :
         (token.type == TOK_GREATER) ? " >= " :
-        (token.type == TOK_PLUS && expType != TYPE_STRING) ? "+" :
-        (token.type == TOK_PLUS && expType == TYPE_STRING) ? ".." :
+        (token.type == TOK_PLUS) ? "+" :
         (token.type == TOK_MINUS) ? "-" :
         (token.type == TOK_MUL) ? "*" :
         (token.type == TOK_DIV) ? "/" :
         "%";
-    return
-        (token.type == TOK_OR) ? ("_or(" + left + ", " + right + ")") : 
-        (token.type == TOK_AND) ? ("_and(" + left + ", " + right + ")") :
-        (left + op + right);
+    return left + op + right;
 }
 
 string Generator::GenUnaryExp(const Token& token, const string& exp) const {
-    if (token.type == TOK_NOT) return ("_not(" + exp + ")") ;
+    if (token.type == TOK_NOT) return ("!" + exp) ;
     else return "-" + exp;
 }
 
@@ -132,7 +111,7 @@ string Generator::GenLiteral(const Token& token) const {
     case TOK_STRINGLITERAL:
         return "\"" + token.data + "\"";
     case TOK_NULLLITERAL:
-        return "nil";
+        return "null";
     default:
         return "<unknown>";
     }
@@ -148,7 +127,7 @@ string Generator::GenIndent(int level) const {
 }
 
 string Generator::GenFunctionHeader(const Function& func) {
-    return "function " + GenFuncId(func.name) + GenParams(func) + "\n";
+    return "function " + GenFuncId(func.name) + GenParams(func);
 }
 
 string Generator::GenParams(const Function& func) {
